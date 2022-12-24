b1778e07d146c691595f6c2994e42c4792a5d353
stf1204 <stf1204@163.com>
2022-12-24 10:01:52 +0000

    删除文件 a.txt

---
+++ src/main/java/HbaseUtil.java
@@ -0,0 +1,16 @@
+/**
+ * @ClassName: HbaseUtil
+ * @PACKAGE: PACKAGE_NAME
+ * @Author: stf
+ * @Date: 2022/12/24 - 18:03
+ * @Description:
+ * @Version: v1.0
+ */
+public class HbaseUtil {
+    public static void main(String[] args) {
+
+    }
+
+    public void test(){}
+
+}
